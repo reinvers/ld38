8e04b0e58235a5f94d7a93f004cb5e76d2ad0e92
reinvers <reinvers@protonmail.com>
2017-04-22 20:59:08 +0700

    Player rotate along with planet

--- player.lua
+++ player.lua
@@ -9,7 +9,13 @@ function player.initialize(planet)
 end
 
 function player.update(dt)
+  local two_pi = math.pi * 2
   
+  player.rotation = player.rotation + player.planet.rotational_speed * dt
+  
+  if player.rotation > two_pi then
+    player.rotation = player.rotation - two_pi
+  end
 end
 
 function player.draw()
