d0a18c1cfe8c8ef9a4ba8feae27c1c84400aaf93
reinvers <reinvers@protonmail.com>
2017-04-22 20:35:19 +0700

    Planet has its own rotation

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ local player = require 'player'
 function love.load()
   love.graphics.setBackgroundColor(10, 20, 50)
   orbit.initialize(sun, 0.75, 1.25, math.pi / 4, 75, -75)
-  planet.initialize(orbit, sun, 1)
+  planet.initialize(orbit, sun, 1, math.pi)
 end
 
 function love.update(dt)
--- planet.lua
+++ planet.lua
@@ -5,11 +5,14 @@ planet.orbit = nil
 planet.sun = nil
 planet.position = 0
 planet.orbital_speed = 0
+planet.rotation = 0
+planet.rotational_speed = 0
 
-function planet.initialize(orbit, sun, orbital_speed)
+function planet.initialize(orbit, sun, orbital_speed, rotational_speed)
   planet.orbit = orbit
   planet.sun = sun
   planet.orbital_speed = orbital_speed
+  planet.rotational_speed= rotational_speed
 end
 
 function planet.update(dt)
@@ -20,6 +23,12 @@ function planet.update(dt)
   if planet.position > two_pi then
     planet.position = planet.position - two_pi
   end
+  
+  planet.rotation = planet.rotation + planet.rotational_speed * dt
+  
+  if planet.rotation > two_pi then
+    planet.rotation = planet.rotation - two_pi
+  end
 end
 
 function planet.draw()
@@ -28,7 +37,7 @@ function planet.draw()
   local rotated_x = planet.sun.x + planet.orbit.offset_x + math.cos(planet.orbit.rotation) * x - math.sin(planet.orbit.rotation) * y
   local rotated_y = planet.sun.y + planet.orbit.offset_y + math.sin(planet.orbit.rotation) * x + math.cos(planet.orbit.rotation) * y
   
-  love.graphics.draw(planet.image, rotated_x, rotated_y, 0, 1, 1, planet.image:getWidth() / 2, planet.image:getHeight() / 2)
+  love.graphics.draw(planet.image, rotated_x, rotated_y, planet.rotation, 1, 1, planet.image:getWidth() / 2, planet.image:getHeight() / 2)
 end
 
 return planet
