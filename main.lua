315ac620a6bb84b4043183f1264c0c30998871d7
reinvers <reinvers@protonmail.com>
2017-04-22 19:04:03 +0700

    Planet automatically moves along its orbit

--- main.lua
+++ main.lua
@@ -5,7 +5,11 @@ local planet = require 'planet'
 function love.load()
   love.graphics.setBackgroundColor(10, 20, 50)
   orbit.initialize(sun, 0.75, 1.25, 1)
-  planet.initialize(orbit, sun)
+  planet.initialize(orbit, sun, 1)
+end
+
+function love.update(dt)
+  planet.update(dt)
 end
 
 function love.draw()
--- planet.lua
+++ planet.lua
@@ -4,17 +4,29 @@ planet.image = love.graphics.newImage('images/planet.png')
 planet.orbit = nil
 planet.sun = nil
 planet.position = 0
+planet.orbital_speed = 0
 
-function planet.initialize(orbit, sun)
+function planet.initialize(orbit, sun, orbital_speed)
   planet.orbit = orbit
   planet.sun = sun
+  planet.orbital_speed = orbital_speed
+end
+
+function planet.update(dt)
+  local two_pi = math.pi * 2
+  
+  planet.position = planet.position + planet.orbital_speed * dt
+  
+  if planet.position > two_pi then
+    planet.position = planet.position - two_pi
+  end
 end
 
 function planet.draw()
   local x = planet.orbit.radius * math.cos(planet.position) * planet.orbit.scale_x
   local y = planet.orbit.radius * math.sin(planet.position) * planet.orbit.scale_y
   local rotated_x = planet.sun.x + math.cos(planet.orbit.rotation) * x - math.sin(planet.orbit.rotation) * y
-  local rotated_y = planet.sun.y + math.sin(planet.orbit.rotation) * x - math.cos(planet.orbit.rotation) * y
+  local rotated_y = planet.sun.y + math.sin(planet.orbit.rotation) * x + math.cos(planet.orbit.rotation) * y
   
   love.graphics.draw(planet.image, rotated_x, rotated_y, 0, 1, 1, planet.image:getWidth() / 2, planet.image:getHeight() / 2)
 end
